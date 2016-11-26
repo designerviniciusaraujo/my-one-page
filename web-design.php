11dfd2455c07fc06967431fa74465337911a426f
Vinicius Araujo <designerviniciusaraujo@gmail.com>
2016-11-26 15:37:49 -0200

    Add files via upload

---
+++ web-design.php
@@ -0,0 +1,138 @@
+ 
+  
+<!-- SESSÃO DESENVOLVIMENTO WEB -->
+    <div class="container-fluid primaria">
+        <div class="row">
+            <section>
+                <div class="dev-web">
+                     
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 primaria">
+                              
+                                <p class="sub">OTIMIZAÇÃO DE BUSCA (SEO).</p>
+                                <h2 class="hidden-xs">Desenvolvimento de aplicações <br>web & mobile. </h2>
+                                <h2 class="hidden-sm hidden-md hidden-lg">Desenvolvimento de web sites.</h2>
+
+                                <hr class="line">
+                 
+                                <p>Posso desenvolver seu catálogo online ou web site, vai lhe ajudar a atender melhor seus clientes, tirar dúvidas e explicar tudo sobre seu produto ou serviço. 
+                                </p>
+
+                                <img class="responsive mob hidden-lg hidden-md" src="img/m-mockup-hotel.png" alt="Job Web Portfolio Responsive">
+
+                                <br><br> <p><span class="hidden-xs hidden-sm">Dedique um tempo a sua apresentação e conquiste novos leads, com landing pages para captação ou conversão (Venda). Campanhas de e-mail marketing pode lhe ajudar a vender novos produtos, conquistar prospectos ou até mesmo apresentar sua marca. <br></span><br>
+
+                                <span class="wide hidden-xs">Se preciso fale comigo, meu desenvolvimento é eficaz e de auto desempenho e a entrega é descomplicada.</span></p>
+                                <ul class="ferer">
+                                    <li><strong> LINGUAGENS E FERRAMENTAS DE TRABALHO:</strong></li><br>
+                                    <li class="min">>>> Html5, Css3, Sass, linux, Jquery, Php jr.</li>
+                                </ul>
+
+                                 <!-- Avançar Section-->
+                                  <div class="row">
+                                      <div class="col-md-12 col-sm-12 col-xs-12">
+                                          <div class="b-cont">                                      
+                                               
+
+                                                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
+                                                  <g>
+                                                    <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751   c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0   c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z" fill="#ffffff"/>
+                                                  </g>
+                                                   
+                                                  </svg>
+                                      
+
+                                          </div>  
+                                        </div>
+                                    </div>
+                                <!-- Avançar Section-->
+
+
+
+                                  <div class="selecao"> 
+
+                                          
+                                        <button class="port btn branco"><a href="projetos-web.php"><span>Cases</span></a></button>
+
+
+                                        <a class="ancora_menu" href="index.php#email">  <button class="port btn cinza">
+                                          <span>Contrate-me</span></button></a>
+
+                                         
+                                              <div class="row">
+                                                    
+                                                      <ul class="veja hidden-xs">
+                                                          <li><a class="botao slin " type="button" data-toggle="modal" data-target="#detalhes-web"><span>» Detalhes do projeto</span></a></li> 
+                                                          <li><a class="botao slin" href="http://www.pousadadascachoeiras.com.br/" target="_blank"><span>» Ver projeto</span></a></li>
+
+                                                          <li><a  class="botao slin " type="button" data-toggle="modal" data-target="#mockup"><span>» Download Mockup</span></a></li> 
+                                                          <li><a  class="botao slin " type="button" data-toggle="modal" data-target="#sugestao"><span>» Sugestões</span></a></li> 
+                                                          
+                                                      </ul>                   
+                               
+
+                           
+                                              </div>
+
+                                      </div><!-- / Seleção-->
+                                
+
+                          </div><!-- /div col-md-6 -->
+
+                          <div class="hidden-xs col-sm-5 col-md-5 col-lg-6">
+                              
+                                 
+                                 <div class="job">
+                                      <span class="space">
+                                         <p>Web hotel</p>
+                                         <h3>Pousada das Cachoeiras</h3>
+                                         <p> <strong class="nip">Desenvolvimento web.</strong></p>
+                                       </span>
+
+                                      <span class="back">
+                                         <p class="drop">último projeto <span style="font-family:'Passion'; color:#cc9a00;">..</span><span style="font-family:'Passion'; color:#fff;">..</span></p>
+                                         <img class="hidden-xs responsive" src="img/mockup-hotel.png">
+                                         <img class="visible-xs hidden-lg" src="img/m-mockup-hotel.png">
+                                      </span>
+                                   
+                                   <!-- Avançar Section-->
+                                  <div class="row">
+                                      <div class="col-md-12 col-sm-12 col-xs-12">
+                                          <div class="b-cont">                                      
+                                                
+                                                
+
+                                               <a class="botao ancora_menu" href="index.php#design"><img src="img/web-continuar.png" alt="icone continuar"></a>  
+                                             
+
+                                          </div>  
+                                        </div>
+                                    </div>
+                                <!-- Avançar Section-->
+                               </div>
+
+                               <div class="row"> 
+                                  <div class="ctn">
+                                    <div class="col-xs-4 col-md-4 col-md-offset-3">
+                                       
+                                    </div>
+                                  </div>
+                               </div> 
+
+
+                          </div><!-- /div col-md-6 -->
+        
+                                   
+                                  
+                              
+                                  
+                            
+                        </div> <!-- DIV 50% / mobile full-->
+ 
+                </div>
+            </section>
+        </div>  <!-- Row -->     
+    </div> <!-- Container Fluid-->
+
+ 
+
+ 
