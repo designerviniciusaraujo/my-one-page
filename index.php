22abe0c1b4f85364fb54b0ea3e6659768bfab63e
Vinicius Araujo <designerviniciusaraujo@gmail.com>
2016-11-26 14:13:35 -0200

    Index.php
    Index for includes.

---
+++ index.php
@@ -0,0 +1,132 @@
+ <html lang="pt-br">
+ 
+  <?php 
+	  	include("head.php")
+
+	 ?>
+	 
+		<body>
+			 
+		  
+			 <?php 
+
+			 include("header.php");
+
+
+			 ?>
+			 <?php 
+
+			 include("slider.php");
+
+
+			 ?>
+	 
+			 <?php 
+
+			 include("topicos.php");
+
+
+			 ?>
+			 
+			 <?php 
+
+			  include("web-design.php");
+
+
+			 ?>
+			 <div id="design"></div>
+			  <?php 
+
+			  include("design.php");
+
+
+			 ?>
+
+			 
+			  <?php 
+
+				include("surf-session.php"); 
+
+
+			 ?>	
+ 	
+			
+			  <?php 
+
+				include("fotografia.php"); 
+
+
+			 ?>		
+
+			 		 
+			 
+			  <?php 
+
+				include("blog.php");
+
+
+			 ?>
+		 
+		 	
+			  <?php 
+
+				include("contato.php");
+
+
+			 ?>
+		 	 
+			 
+
+			 <?php 
+
+				include("footer.php");
+
+
+			 ?>
+		 	 
+			 
+
+			 
+ 
+ 			 <?php 
+
+				include("modals.php");
+
+
+			 ?>
+ 
+
+
+			
+
+
+ 
+			  	
+ 
+
+			 
+
+	
+ 
+
+ 	<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-71117526-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+ 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+ 
+ 	<script src="js/bootstrap.min.js"></script>
+    <script src="js/js.js"></script>
+	 </body>
+	   
+
+ </html>
